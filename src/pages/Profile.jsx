ddb130af9e5ee58c165d2de7c37fc0f37c9c1e4b
Kevin Willoughby <iamkevinn@protonmail.com>
2022-05-08 20:03:42 -0400

    Sign in functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,17 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getAuth } from 'firebase/auth';
+
 function Profile() {
-  return <div>Profile</div>;
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  const auth = getAuth();
+
+  useEffect(() => {
+    if (auth.currentUser) setUser(auth.currentUser);
+  }, []);
+
+  return user ? <h1>{user.displayName}</h1> : 'Not logged in';
 }
 export default Profile;
