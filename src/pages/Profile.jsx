00704396bb01972706ceb21631fb2078da9eb47b
Kevin Willoughby <iamkevinn@protonmail.com>
2022-05-09 19:26:14 -0400

    Rent page update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { getAuth, updateProfile } from 'firebase/auth';
 import { db } from '../firebase.config';
 import { doc, updateDoc } from 'firebase/firestore';
